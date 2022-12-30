87320f0351e39744b3b46c19fc3b802d23f613f0
Uros Dasic <uros1992dasic@gmail.com>
2022-12-30 13:42:27 +0100

    add desktop and movile navigation

--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,8 +1,12 @@
 import React from "react";
 
+import './SideDrawer.css';
+
 const SideDrawer = (props) => {
     return (
-        <></>
+        <aside className="side-drawer">
+            {props.children}
+        </aside>
     );
 }
 
