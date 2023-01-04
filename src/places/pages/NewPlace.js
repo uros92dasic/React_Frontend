3e0a69800fb0ce518520ccfeed0f13a115f7e738
Uros Dasic <uros1992dasic@gmail.com>
2023-01-04 14:05:16 +0100

    finish the add place form

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -53,10 +53,15 @@ const NewPlace = () => {
         });
     }, []);
 
+    const placeSubmitHandler = event => {
+        event.preventDefault();
+        console.log(formState.inputs); //send this to the backend
+    };
+
     return (
         <>
         <h3>New Place!</h3>
-        <form className="place-form">
+        <form className="place-form" onSubmit={placeSubmitHandler}>
             <Input
                 id="title"
                 element="input"
@@ -73,6 +78,15 @@ const NewPlace = () => {
                 label="Title"
                 validators={[VALIDATOR_MINLENGTH(5)]}
                 errorText="Please enter a valid description (at least 5 characters)."
+                onInput={inputHandler}
+            />
+            <Input
+                id="address"
+                element="input"
+                type="text"
+                label="Address"
+                validators={[VALIDATOR_REQUIRE()]}
+                errorText="Please enter a valid address."
                 onInput={inputHandler}
             />
             <Button type="submit" disabled={!formState.isValid}>ADD PLACE</Button>
