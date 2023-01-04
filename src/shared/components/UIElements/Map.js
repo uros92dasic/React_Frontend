ccbaf71a569a9abfe2cb7ccadf67880a350ead56
Uros Dasic <uros1992dasic@gmail.com>
2023-01-04 10:36:00 +0100

    add rendering a map

---
+++ src/shared/components/UIElements/Map.js
@@ -0,0 +1,44 @@
+import React, { useRef, useEffect } from 'react';
+ 
+import './Map.css';
+ 
+const Map = props => {
+//   const mapRef = useRef();
+  
+//   const { center, zoom } = props;
+ 
+//   useEffect(() => {
+//     new window.ol.Map({
+//       target: mapRef.current.id,
+//       layers: [
+//         new window.ol.layer.Tile({
+//           source: new window.ol.source.OSM()
+//         })
+//       ],
+//       view: new window.ol.View({
+//         center: window.ol.proj.fromLonLat([center.lng, center.lat]),
+//         zoom: zoom
+//       })
+//     });
+//   }, [center, zoom]);
+ 
+//   return (
+//     <div
+//       ref={mapRef}
+//       className={`map ${props.className}`}
+//       style={props.style}
+//       id="map"
+//     ></div>
+//   );
+
+//https://console.cloud.google.com/freetrial/signup/tos - GoogleMapIDE
+
+    return (
+        <>
+        <h2>This is the "RED MAP"</h2>
+        <div className='red'></div>
+        </>
+    );
+};
+ 
+export default Map;
