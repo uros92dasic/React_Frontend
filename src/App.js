d716105ee83fbc6e55b551f3eb44d9afe1e082cc
Uros Dasic <uros1992dasic@gmail.com>
2023-01-05 13:09:50 +0100

    add auth and unauthenticated routes

--- src/App.js
+++ src/App.js
@@ -19,32 +19,49 @@ function App() {
     setIsLoggedIn(false);
   }, []);
 
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
+      <Switch>
+      <Route path="/" exact>
+        <Users />
+      </Route>
+      <Route path="/:userId/places" exact>
+        <UserPlaces />
+      </Route>
+      <Route path="/places/new" exact>
+        <NewPlace />
+      </Route>
+      <Route path="/places/:placeId">
+        <UpdatePlace />
+      </Route>
+      <Redirect to="/" />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+      <Route path="/" exact>
+        <Users />
+      </Route>
+      <Route path="/:userId/places" exact>
+        <UserPlaces />
+      </Route>
+      <Route path="/auth">
+        <Auth></Auth>
+      </Route>
+      <Redirect to="/auth" />
+      </Switch>
+    );
+  }
+
   return (
     <AuthContext.Provider value={{isLoggedIn: isLoggedIn, login: login, logout: logout}}>
     <Router>
       <MainNavigation />
         <main>
-        <Switch>
-          <Route path="/" exact>
-            <Users />
-          </Route>
-          <Route path="/:userId/places" exact>
-            <UserPlaces />
-          </Route>
-          <Route path="/places/new" exact>
-            <NewPlace />
-          </Route>
-          <Route path="/places/:placeId">
-            <UpdatePlace />
-          </Route>
-          <Route path="/auth">
-            <Auth></Auth>
-          </Route>
-          <Route path="*">
-            <Page404 />
-          </Route>
-          <Redirect to="/" />
-        </Switch>
+          {routes}
         </main>
     </Router>
     </AuthContext.Provider>
--- src/places/pages/Auth.js
+++ src/places/pages/Auth.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Button from "../../shared/components/FormElements/Button";
 import Input from "../../shared/components/FormElements/Input";
 
+import { AuthContext } from "../../shared/context/auth-context";
 import { useForm } from "../../shared/hooks/form-hook";
 import Card from "../../shared/components/UIElements/Card";
 import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/util/validators";
 import './Auth.css';
 
 const Auth = () => {
+    const auth = useContext(AuthContext);
+
     const [isLoginMode, setIsLoginMode] = useState(true);
 
     const [formState, inputHandler, setFormData] = useForm({
@@ -24,6 +27,7 @@ const Auth = () => {
     const authSubmitHandler = (event) => {
         event.preventDefault();
         console.log(formState);
+        auth.login();
     }
 
     const switchModeHandler = () => {
